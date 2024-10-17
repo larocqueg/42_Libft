b7da4ef676a11716f52ade7b8f2cd88ac3f76740
laroqueg <portugalgabriel2021@gmail.com>
2024-10-17 19:47:29 +0100

    added all functions to the libft.h and the makefile

--- Libft/includes/libft.h
+++ Libft/includes/libft.h
@@ -6,51 +6,21 @@
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:26:47 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/10/17 16:28:24 by larocqueg        ###   ########.fr       */
+/*   Updated: 2024/10/17 19:43:41 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int		ft_atoi(const char *str);
-int		ft_isalnum(int i);
-int		ft_isalpha(int i);
-int		ft_isascii(int i);
-int		ft_isdigit(int i);
-int		ft_isprint(int i);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strlen(const char *str);
-int		ft_strncmp(const char *s1, char *s2, size_t n);
-int		ft_tolower(int i);
-int		ft_toupper(int i);
-
-void	ft_bzero(void *s, size_t n);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-
-void	*ft_calloc(size_t count, size_t size);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *s1, const void *s2, size_t n);
-void	*ft_memset(void *b, int c, size_t len);
-
-char	*ft_strchr(const char *s, int i);
-char	*ft_strdup(const char *s);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
-char	**ft_split(char const *str, char c);
-
-size_t	ft_strlcat(char *dest, const char *src, size_t size);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+int	ft_isalnum(int i);
+int	ft_isalpha(int i);
+int	ft_isascii(int i);
+int	ft_isdigit(int i);
+int	ft_isprint(int i);
+int	ft_strchr(int i);
+int	ft_strlen(char *str);
+int	ft_tolower(int i);
+int	ft_toupper(int i);
 
 #endif
--- Libft/srcs/ft_isdigit.c
+++ Libft/srcs/ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 17:39:34 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/10/17 17:44:00 by larocqueg        ###   ########.fr       */
+/*   Updated: 2024/10/17 19:42:56 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int i);
+int	ft_isdigit(int i)
 {
 	if (i >= '0' && i <= '9')
 		return (0);
