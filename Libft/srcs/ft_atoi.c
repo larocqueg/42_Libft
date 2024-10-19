cb36818577c11eed57c2ea581c4483fa8318e76f
laroqueg <portugalgabriel2021@gmail.com>
2024-10-19 12:27:19 +0100

    ft_atoi.c

--- Libft/srcs/ft_atoi.c
+++ Libft/srcs/ft_atoi.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 19:19:44 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/10/19 12:26:46 by larocqueg        ###   ########.fr       */
+/*   Created: 2024/10/19 12:06:41 by larocqueg         #+#    #+#             */
+/*   Updated: 2024/10/19 12:26:20 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int i)
+int	ft_atoi(const char *str)
 {
-	if (i >= 'A' && i <= 'Z')
-		return (i + 32);
-	else
-		return (i);
+	int	n;
+	int	sign;
+
+	sign = 1;
+	if (!str)
+		return (0);
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if ((*str == '-' || *str == '+'))
+	{
+		if (*str == '-')
+			sign = -sign;
+		str++;
+		if (*(str + 1) == '-' || *(str + 1) == '+')
+			return (0);
+	}
+	n = 0;
+	while (ft_isdigit(*str))
+		n = n * 10 + (*str++ - '0');
+	return (n * sign);
 }
