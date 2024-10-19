a943021581e4aec482b0d8c260eb781a4858938e
laroqueg <portugalgabriel2021@gmail.com>
2024-10-19 14:15:51 +0100

    norminetted

---
+++ Libft/src/ft_memcpy.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 13:49:33 by larocqueg         #+#    #+#             */
+/*   Updated: 2024/10/19 14:13:59 by larocqueg        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(unsigned char *dest, unsigned char *src, size_t n)
+{
+	unsigned char	*src_cpy;
+	unsigned char	*dest_cpy;
+	size_t			i;
+}
