09b1f0cbd8451b6f700064f3ef7cd1d4afc7e356
laroqueg <portugalgabriel2021@gmail.com>
2024-10-19 12:49:40 +0100

    strduo + include

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,21 +6,30 @@
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:26:47 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/10/17 19:43:41 by larocqueg        ###   ########.fr       */
+/*   Updated: 2024/10/19 12:46:12 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalnum(int i);
-int	ft_isalpha(int i);
-int	ft_isascii(int i);
-int	ft_isdigit(int i);
-int	ft_isprint(int i);
-int	ft_strchr(int i);
-int	ft_strlen(char *str);
-int	ft_tolower(int i);
-int	ft_toupper(int i);
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <math.h>
+# include <limits.h>
+
+int		ft_isalnum(int i);
+int		ft_isalpha(int i);
+int		ft_isascii(int i);
+int		ft_isdigit(int i);
+int		ft_isprint(int i);
+int		ft_strchr(int i);
+int		ft_strlen(char *str);
+int		ft_tolower(int i);
+int		ft_toupper(int i);
+int		ft_atoi(char *str);
+char	*ft_strdup(char *str);
 
 #endif
