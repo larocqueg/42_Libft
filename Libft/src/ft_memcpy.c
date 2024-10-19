ec49e091627679672c066bdedd597fa1bdb4e26e
laroqueg <portugalgabriel2021@gmail.com>
2024-10-19 14:39:39 +0100

    memcpyy

--- Libft/src/ft_memcpy.c
+++ Libft/src/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: larocqueg <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:49:33 by larocqueg         #+#    #+#             */
-/*   Updated: 2024/10/19 14:13:59 by larocqueg        ###   ########.fr       */
+/*   Updated: 2024/10/19 14:24:17 by larocqueg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void	*ft_memcpy(unsigned char *dest, unsigned char *src, size_t n)
 {
-	unsigned char	*src_cpy;
-	unsigned char	*dest_cpy;
-	size_t			i;
+	size_t	i;
+
+	if (!src || !dest)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return ((void *)dest);
 }
