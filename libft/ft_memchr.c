5766dd771283b6802c718d08a3a98a51e0b64fe6
Gabriel Aguiar De La Rocque <gde-la-r@c2r4s4.42porto.com>
2024-10-26 17:58:53 +0100

    memchr main done

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:02:10 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/23 15:18:23 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/26 17:27:29 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,23 @@ void	*ft_memchr(const void *s, int c, size_t n)
 		i++;
 	}
 	return (NULL);
+}
+/*
+#include "ft_strlen.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	char	*str = av[1];
+	const void	*stv = str;
+	int	i = av[2][0];
+	if (ac == 3)
+	{
+		printf("%s\n", (char *)ft_memchr(str, i, ft_strlen(av[1]) + 1));
+		return (1);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (0);
 }
