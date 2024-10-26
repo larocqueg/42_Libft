0020c6c70f9166635a0a4867e532118f7a22b6a3
larocqueg <portugalgabriel2021@gmail.com>
2024-10-26 23:17:18 +0100

    putstr_fd main done

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -18,8 +18,18 @@ void	ft_putstr_fd(char *s, int fd)
 		ft_putchar_fd(*s++, fd);
 }
 /*
+#include "ft_putchar_fd.c"
 int	main(int ac, char **av)
 {
-	(void)ac;
-	ft_putstr_fd(av[1], 1);
+	if (ac == 2)
+	{
+		ft_putstr_fd(av[1], 1);
+		ft_putchar_fd('\n', 1);
+		return (0);
+	}
+	else if (ac > 2)
+		ft_putstr_fd("Too many arguments!\n", 1);
+	else
+		ft_putstr_fd("Missing command-line argument!\n", 1);
+	return (1);
 }*/
