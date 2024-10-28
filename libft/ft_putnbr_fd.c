c915addafc710b54f8ef8b5fcbc753f0cc76c739
Gabriel Aguiar De La Rocque <gde-la-r@c1r12s4.42porto.com>
2024-10-28 11:11:07 +0000

    putnbr main done

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:59:25 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/25 18:09:11 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/28 11:10:49 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,21 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(nb / 10, fd);
 	ft_putchar_fd((nb % 10) + '0', fd);
 }
+/*
+#include "ft_atoi.c"
+#include "ft_putchar_fd.c"
+#include "ft_putstr_fd.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		ft_putnbr_fd((int)ft_atoi(av[1]), (int)ft_atoi(av[2]));
+		return (0);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
