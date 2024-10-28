f7cfc15414f6797b3cf046d58ceca7d33ba50ed2
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s9.42porto.com>
2024-10-28 16:17:15 +0000

    last commit of the day

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,33 +6,38 @@
 /*   By: gde-la-r <gde-la-r@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:41:53 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/23 14:35:33 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/28 15:49:51 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	int		len;
 	size_t	i;
-	size_t	j;
-	size_t	res_d;
-	size_t	res_s;
 
-	i = ft_strlen(dest);
-	j = 0;
-	res_d = i;
-	res_s = ft_strlen(src);
-	if (size < 1)
-		return (res_s + size);
-	while (src[j] && i < size - 1)
+	i = 0;
+	if (size < ft_strlen(dst))
+		return (size + ft_strlen(src));
+	else
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		while (dst[i])
+			i++;
+		ft_strlcpy(&dst[i], src, size - i);
+		len = ft_strlen(src) + i;
+		return (len);
 	}
-	dest[i] = '\0';
-	if (size < res_d)
-		return (res_s + size);
-	return (res_d + res_s);
 }
+/*
+#include <stdio.h>
+#include "ft_strlcpy.c"
+#include "ft_strlen.c"
+int main()
+{
+	char dest1[11] = "Hello";
+	const char *src1 = "World";
+	printf("%zu\n", ft_strlcat(dest1, src1, sizeof(dest1)));
+	printf("Result: %s\n", dest1);
+	return 0;
+}*/
