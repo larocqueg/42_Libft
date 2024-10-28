786ff3559fa3d1b37d063b00402dbf520dacb14a
Gabriel Aguiar De La Rocque <gde-la-r@c1r12s4.42porto.com>
2024-10-28 10:39:37 +0000

    ft_memset main done

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <gde-la-r@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:17:39 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/21 16:23:17 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/28 10:39:22 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,19 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return ((void *)b);
 }
+/*
+#include "ft_atoi.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		printf("%s\n", (char *)ft_memset(av[1], av[2][0], ft_atoi(av[3])));
+		return (0);
+	}
+	else if (ac > 4)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
