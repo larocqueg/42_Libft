31e9d135d226a9888fb644c0e1e5b809b1eaec94
larocqueg <portugalgabriel2021@gmail.com>
2024-10-29 13:23:46 +0000

    ft_itoa norminetted

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,50 +12,54 @@
 
 #include "libft.h"
 
-static int ft_numlen(int n)
+static int	ft_numlen(int n)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    if (n <= 0)
-        len++;
-    while (n != 0)
-    {
-        n /= 10;
-        len++;
-    }
-    return (len);
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
 }
 
-char *ft_itoa(int n)
+static char	*ft_completestr(int n, size_t len, size_t neg)
 {
-    size_t len;
-    size_t neg;
-    char *str;
+	str = (char *)malloc(sizeof(char) * (len + neg + 1));
+	if (!str)
+		return (NULL);
+	if (neg == 1)
+		str[0] = '-';
+	str[len + neg] = '\0';
+	len--;
+	while (n >= 10)
+	{
+		str[len + neg] = (n % 10) + '0';
+		n /= 10;
+		len--;
+	}
+	str[len + neg] = n + '0';
+	return (str);
+}
 
-    if (n == -2147483648)
-        return (ft_strdup("-2147483648"));
-    neg = 0;
-    if (n < 0)
-    {
-        n = -n;
-        neg = 1;
-    }
-    len = ft_numlen(n);
-    str = (char *)malloc(sizeof(char) * (len + neg + 1));
-    if (!str)
-        return (NULL);
-    if (neg == 1)
-        str[0] = '-';
-    str[len + neg] = '\0';
-    len--;
-    while (n >= 10)
-    {
-        str[len + neg] = (n % 10) + '0';
-        n /= 10;
-        len--;
-    }
-    str[len + neg] = n + '0';
+char	*ft_itoa(int n)
+{
+	size_t	len;
+	size_t	neg;
+	char	*str;
 
-    return (str);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	neg = 0;
+	if (n < 0)
+	{
+		n = -n;
+		neg = 1;
+	}
+	len = ft_numlen(n);
+	return (ft_completestr(n, len, neg));
 }
