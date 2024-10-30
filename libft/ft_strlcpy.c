1fdce57d75f43c302a8f94fc0dc48f1cef387896
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 11:32:15 +0000

    ft_strlcpy main done

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:58:48 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/23 12:25:04 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:28:27 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,21 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 		i++;
 	return (i);
 }
+/*
+#include "ft_strlen.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		printf("ft_strlcpy(av[1], av[2]) = %zu\n",
+		ft_strlcpy(av[1], av[2], ft_strlen(av[2]) + 1));
+		printf("av[1] turned into (%s)\n", av[1]);
+		return (0);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
