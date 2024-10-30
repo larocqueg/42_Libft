57db5536f5ff09ab07ce2ae607dbc840232029ec
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 14:38:08 +0000

    substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:35:01 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/24 11:35:02 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:37:58 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,3 +37,23 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	dest[i] = '\0';
 	return (dest);
 }
+/*
+#include "ft_strlen.c"
+#include "ft_strdup.c"
+#include "ft_atoi.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		char *dest = ft_substr(av[1], ft_atoi(av[2]), ft_strlen(av[1]));
+		printf("%s\n", dest);
+		free(dest);
+		return (0);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
