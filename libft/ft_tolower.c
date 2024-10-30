0a02196a877e1ac92901ee3a180cbba6d0c12002
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 14:44:03 +0000

    all main done

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <gde-la-r@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:58:22 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/21 12:58:23 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:41:19 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,21 @@ int	ft_tolower(int i)
 	else
 		return (i);
 }
+/*
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int	i = 0;
+
+		while (av[1][i])
+			printf("%c", ft_tolower(av[1][i++]));
+		return (0);
+	}
+	else if (ac > 2)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <gde-la-r@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:58:30 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/21 12:58:31 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:43:46 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,20 @@ int	ft_toupper(int i)
 	else
 		return (i);
 }
+/*
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int	i = 0;
+		while (av[1][i])
+			printf("%c", ft_toupper(av[1][i++]));
+		return (0);
+	}
+	else if (ac > 2)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
