2832639a9ea8acc8739f26a8ea51689311614cfa
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 11:49:47 +0000

    strmstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:06:22 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/28 18:33:36 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:49:38 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,3 +40,19 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
+/*
+#include "ft_strlen.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		printf("%s\n", ft_strnstr(av[1], av[2], ft_strlen(av[1])));
+		return (0);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
