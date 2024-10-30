8184e650281dec3df9a5a0a437f9c07f75302ec0
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 11:05:27 +0000

    strjoin main done

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:34:35 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/24 11:34:51 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:05:09 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
 	int		i;
 	int		s1_len;
@@ -40,3 +40,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	return (NULL);
 }
+/*
+#include "ft_strlen.c"
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		char	*dest;
+		dest = ft_strjoin(av[1], av[2]);
+		printf("%s\n", dest);
+		free (dest);
+		return (0);
+	}
+	else if (ac > 3)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
