94bbe127cce64a6c443db153303515dd34567d88
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-10-30 11:34:51 +0000

    strlen main done

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <gde-la-r@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:58:12 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/10/22 16:18:50 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:34:38 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,18 @@ size_t	ft_strlen(const char *str)
 		i++;
 	return (i);
 }
+/*
+#include <stdio.h>
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		printf("The len of av[1] is = %zu\n", ft_strlen(av[1]));
+		return (0);
+	}
+	else if (ac > 2)
+		printf("Too many arguments!\n");
+	else
+		printf("Missing command-line argument!\n");
+	return (1);
+}*/
